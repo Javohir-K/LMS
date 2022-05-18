6e317cc2826d97ef317bbc52c5fed387021e2eb7
Javohir <61010500+Javohir-K@users.noreply.github.com>
2022-05-19 00:33:02 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhtS9yHvb-7w16vjB7bW0yoVm9NeHOfD4",
-  authDomain: "lms-edec.firebaseapp.com",
-  projectId: "lms-edec",
-  storageBucket: "lms-edec.appspot.com",
-  messagingSenderId: "65831391459",
-  appId: "1:65831391459:web:1fd23ed5f8572b1c9cf5d6",
-  measurementId: "G-PM70XYBL2B"
+  //Here is your firebase api keys
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
